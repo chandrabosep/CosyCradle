ab97fd632968de1deb319e31f3fa90d364cdd1af
chandrabosep <chandrabosep3112@gmail.com>
2023-01-26 12:05:43 +0530

    Added Navbar ❤

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-    <h1 className="text-3xl font-bold underline mx-10">
-      Hello world!
-    </h1>
+    <Navbar/>
     </>
   );
 }
