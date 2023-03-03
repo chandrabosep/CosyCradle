0c6bf366151200c610a2bc2637185838e9440ee4
chandrabosep <chandrabosep3112@gmail.com>
2023-03-03 20:37:45 +0530

    Functionality added 🎉

--- src/components/Soundcard.jsx
+++ src/components/Soundcard.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import "./Soundcard.css";
 
-
-function changeVolume(){
-    
-}
-
 export default function Soundcard(props) {
+  var audio = useRef(new Audio(props.audio))
+  var [InputDisplay,setInputDisplay] = useState(false)
+  
+  function handelPlay(event){
+    if (InputDisplay===false) {
+      audio.current.play()
+      audio.current.loop=true
+      setInputDisplay(true)
+    }
+    else{
+      setInputDisplay(false)
+      audio.current.pause()
+    }
+    event.target.classList.toggle('audio_img_active')  
+  }
+  
+  function changeVolume(){
+    audio.current.volume=Event.target.range
+  }
+  
   return (
       <div className='audio_card'>
-          <img src={props.image}  alt="alt" className='audio_img'/>
+          <img src={props.image} onClick={handelPlay} alt="alt" className='audio_img'/>
           <input type="range" className='audio_volume' onInput={changeVolume} min="0" max="1" step="0.01" defaultValue="0.5" 
-          style={{visibility:  changeVolume ? ' visible' : 'hidden'}}/>
+          style={{visibility:  InputDisplay ? ' visible' : 'hidden'}}/>
         
       </div>
   )
