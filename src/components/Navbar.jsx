84f888769121276e10c58abe54d502d4a282c6ae
chandrabosep <chandrabosep3112@gmail.com>
2023-03-09 21:12:31 +0530

    Media Quries added👍🏼

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,10 +8,10 @@ export default function Navbar() {
     <nav>
       <div className='flex items-center mx-10 mt-6 text-white justify-between'>
           <div className='flex flex-center'>
-            <span className='lo-go'><img src={logo} alt="logo" /></span><span className='text-2xl font-semibold '>Ponder</span>
+            <span className='lo-go'><img src={logo} alt="logo" /></span><span className='text-lg font-normal ' style={{cursor:'pointer'}}>Ponder</span>
           </div>
           <div className='float-right'>
-            <span>
+            <span style={{cursor:'pointer'}} className="text-lg">
               About
             </span>
           </div>
