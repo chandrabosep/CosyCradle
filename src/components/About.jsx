3ffa3aacbaaf9b9109de0a4e4ec2a668e3c1536e
chandrabosep <chandrabosep3112@gmail.com>
2023-03-12 18:04:10 +0530

    Changed to CosyCradle

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,13 +7,13 @@ import linkedin from ".././assets/icons/Linkedin.svg"
 
 export default function About() {
   return (
-    <div class="flex justify-center items-center text-slate-300 text-lg">
+    <div class="flex justify-center items-center text-slate-200 text-lg">
         <div class="w-8/12 ">
         <div className='about'>
-            <p className='text-4xl mb-7'>What is Ponder ?</p><br />
-            <p className='leading-6'>Looking to boost your concentration and focus? Ponder is the ultimate digital platform for you.</p><br />
-           <p className='leading-6'>With Ponder, Create your own personalized sound environment on Ponder. Choose from 20 high-quality background sounds to mix and match, adjusting the volume of each to customize your soundscape. Enjoy a unique experience tailored to your preferences.</p><br />
-           <p className='about-3 leading-6'>If you're not sure where to begin, Ponder offers a variety of sound options to help you find your perfect match. Whether you're studying, working, or just need to relax, Ponder's expertly curated sounds will aid you in staying focused and productive</p>
+            <p className='text-4xl mb-7'>What is CosyCradle ?</p><br />
+            <p className='leading-6'>Looking to boost your concentration and focus? CosyCradle is the ultimate digital platform for you.</p><br />
+           <p className='leading-6'>With CosyCradle, Create your own personalized sound environment. Choose from 20 high-quality background sounds to mix and match, adjusting the volume of each to customize your soundscape. Enjoy a unique experience tailored to your preferences.</p><br />
+           <p className='about-3 leading-6'>If you're not sure where to begin, CosyCradle offers a variety of sound options to help you find your perfect match. Whether you're studying, working, or just need to relax, CosyCradle's expertly curated sounds will aid you in staying focused and productive</p>
            <br /><br /><br />
         </div>
         <div className='about-dev'>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ export default function Navbar() {
       <div className='flex items-center mx-10 mt-6 text-white justify-between'>
           <div className='flex flex-center'>
             <span className='lo-go'><img src={logo} alt="logo" /></span><span className='text-lg font-normal ' style={{cursor:'pointer'}}>
-              <li className='list-none'><Link to="/">Ponder</Link></li>
+              <li className='list-none'><Link to="/">CosyCradle</Link></li>
             </span>
           </div>
           <div className='float-right'>
