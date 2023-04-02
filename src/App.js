9c7b9d06f5382f941fc0f10e0e5c07eccd4a20f1
chandrabosep <chandrabosep3112@gmail.com>
2023-04-02 19:45:17 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from './components/About';
 import Home from './components/Home';
-
+ 
 function App() {
   return (
     <Router>
--- src/components/Soundcard.jsx
+++ src/components/Soundcard.jsx
@@ -30,5 +30,4 @@ export default function Soundcard(props) {
         
       </div>
   )
-  
 }
